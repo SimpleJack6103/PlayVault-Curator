8ef3fc48182c15bfc976689003559124c5826cf0
CryptFormer34 <jackgeraldevans@gmail.com>
2025-04-07 00:20:23 -0700

    updated code 04/06/2025

---
+++ PVC/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Call the main method of DirectorySearch
+        DirectorySearch.main(args);
+
+        // Alternatively, call specific methods directly if needed
+        // Example: DirectorySearch.searchFiles(new File("your_directory_path"));
+    }
+}
